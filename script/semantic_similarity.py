8f32c3f9db818bb06b3c86c1d080248cc25f5c45
Toshiya Komoda <toshiya.komoda@gmail.com>
2017-05-06 20:54:59 +0900

    refactor codes to pass pep8 test.

--- script/semantic_similarity.py
+++ script/semantic_similarity.py
@@ -10,6 +10,7 @@ _MECAB_TOKENIZER = MeCab.Tagger("-Owakati")
 # https://shogo82148.github.io/blog/2015/12/20/mecab-in-python3-final/
 _MECAB_TOKENIZER.parse('')
 
+
 def fetch_all(table_name):
     db_password = 'root'
     if 'DB_PASSWORD' in os.environ:
@@ -33,27 +34,49 @@ def convert_to_snake(name):
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 
+def preprocess(value):
+    snake_case_value = convert_to_snake(value)
+    snake_case_value = re.sub(r'[0-9]', '', snake_case_value)
+    tokens = re.split(r'[-_\]\[ ]', snake_case_value)
+    for t in tokens:
+        japanese_tokens = _MECAB_TOKENIZER.parse(t).split(' ')
+        for j_t in japanese_tokens:
+            if j_t == "\n":
+                continue
+            yield j_t
+
+
 def get_attrs_value(html):
     words = []
     soup = BeautifulSoup(html, 'html.parser')
     s = soup.find('input')
-    for k in ['name', 'type', 'value', 'id', 'alt', 'placeholder']:
-        if (k in s.attrs) and (s.attrs[k] != ''):
-            preprocessed_value = re.sub(r'[0-9]', '', convert_to_snake(s.attrs[k]))
-            tokens = re.split(r'[-_\]\[ ]', preprocessed_value)
-            for t in tokens:
-                p_t = _MECAB_TOKENIZER.parse(t).split(' ')
-                words.extend(p_t[:-1])
+    for k in ['name',
+              'type',
+              'value',
+              'id',
+              'alt',
+              'placeholder']:
+        if (k not in s.attrs) or (s.attrs[k] == ''):
+            continue
+
+        for token in preprocess(s.attrs[k]):
+            words.append(token)
     return words
 
 
 class LsiModel:
     def __init__(self, answers):
         self.answers = answers
-        self.dictionary = corpora.Dictionary(answers)
-        self.corpus = [self.dictionary.doc2bow(answer) for answer in answers]
-        self.lsi = models.LsiModel(self.corpus, id2word=self.dictionary, num_topics=5)
-        self.index = similarities.MatrixSimilarity(self.lsi[self.corpus])
+
+        dictionary = corpora.Dictionary(answers)
+        corpus = [dictionary.doc2bow(answer) for answer in answers]
+        lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=5)
+        index = similarities.MatrixSimilarity(lsi[corpus])
+
+        self.dictionary = dictionary
+        self.corpus = corpus
+        self.lsi = lsi
+        self.index = index
 
     def get_similar_inputs(self, target_tag):
         vec_bow = self.dictionary.doc2bow(get_attrs_value(target_tag))
@@ -66,25 +89,20 @@ class LsiModel:
 def output(test_record, records, similarities):
     print('URL: ' + test_record['url'])
     print('target tag: ' + test_record['html'])
-    # for debug 
-    #print('target words:' + " ".join(get_attrs_value(test_record['html']))) 
     labels = []
     for i in range(0, 5):
         r = records[similarities[i][0]]
         labels.append(r['label'])
-        # for debug 
-        #print('label:' + r['label'] + ' url:' + r['url'] + ' html:' + r['html'] + ' words:' + " ".join(get_attrs_value(r['html'])) + 'similarity: ' + str(similarities[i][1]))
     num_labels = {}
     for label in labels:
         if label not in num_labels:
             num_labels[label] = 1
         else:
             num_labels[label] += 1
-    max_key = max(num_labels.keys(), key=lambda item:num_labels[item])
-    #print('similar labels: ', end='')
-    #print(num_labels)
+    max_key = max(num_labels.keys(), key=lambda item: num_labels[item])
     print('estimated label: ' + max_key)
     print('answer label:' + test_record['label'])
+
 
 def main():
     records = fetch_all('inputs')
@@ -99,7 +117,7 @@ def main():
     for t in test_records:
         target_tag = t['html']
         t_label = t['label']
-        similarities = model.get_similar_inputs(target_tag) 
+        similarities = model.get_similar_inputs(target_tag)
         output(t, records, similarities)
         print()
 
