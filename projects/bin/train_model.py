5f77a240123527beae3711230cd16b128d12c6f2
Toshiya Komoda <toshiya.komoda@gmail.com>
2017-10-19 16:16:39 +0900

    refactoring nn_fc_model.py

--- projects/bin/train_model.py
+++ projects/bin/train_model.py
@@ -3,9 +3,7 @@ import sys
 import os
 import yaml
 import argparse
-from semantic_selector import lsi_model
 from semantic_selector import nn_fc_model
-from semantic_selector import chainer_model
 from semantic_selector import datasource
 
 
@@ -19,6 +17,8 @@ def main():
     parser.add_argument('--ratio_test', type=float, nargs='?', help='a ratio of test sets', default=0.05)
     parser.add_argument('--model_name', nargs='?', help='model to use', default="nn_fc")
     parser.add_argument('--seed', type=int, nargs='?', help='seed of np.random', default=100)
+    parser.add_argument('--epochs', type=int, nargs='?', help='seed of np.random', default=400)
+    parser.add_argument('--debug', action='store_true')
     args = parser.parse_args()
 
     (training, tests) = datasource.InputTags(args.threashold).fetch_data(args.ratio_test, args.seed)
@@ -26,33 +26,25 @@ def main():
     model_name = args.model_name
     print("model type: %s" % (model_name))
     if model_name == "nn_fc":
-        model = nn_fc_model.NNFullyConnectedModel(training, tests)
-    elif model_name == "chainer":
-        model = chainer_model.ChainerModel(training, tests)
-    elif model_name == "lsi":
-        model = lsi_model.LsiModel(training)
+        model = nn_fc_model.NNFullyConnectedModel()
     else:
         print("model %s unknown" % (model_name))
         sys.exit(1)
 
-
-    print("failing inferences\n")
-    print("estimated, correct")
-    positive_hit = 0
-    for t in tests:
-        estimated_label = model.inference_html(t)
-        correct_label = t.label
-        if estimated_label != correct_label:
-            print(estimated_label + "," + correct_label)
-        if estimated_label == correct_label:
-            positive_hit += 1
+    model.train(training, tests, args.epochs)
+    if args.debug:
+        print("failing inferences\n")
+        print("estimated, correct")
+        for t in tests:
+            estimated_label = model.inference_html(t)
+            correct_label = t.label
+            if estimated_label != correct_label:
+                print(estimated_label + "," + correct_label)
 
     print()
     print("# of test data: " + str(len(tests)))
     print("# of training_data: " + str(len(training)))
-    print("Accuracy,", positive_hit / len(tests))
-    print("Recall,", (positive_hit / len(tests)))
-
+    model.save()
 
 if __name__ == '__main__':
     main()
--- projects/semantic_selector/nn_fc_model.py
+++ projects/semantic_selector/nn_fc_model.py
@@ -1,44 +1,55 @@
 import os
+import pickle
 import numpy as np
 import keras
 from gensim import corpora, models, matutils
 from keras.datasets import mnist
 from keras.models import Sequential
+from keras.models import load_model
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
-from semantic_selector import vectorizer
+from semantic_selector import tokenizer
 
 
 class NNFullyConnectedModel:
 
-    def __init__(self, training, tests):
-        self.training_data = vectorizer.Vectorizer(training)
-        self.dictionary = self.training_data.dictionary
-        self.num_terms = self.training_data.num_terms
-        self.num_classes = self.training_data.num_classes
+    def __init__(self):
+        self.batch_size = 200
+        self.model = None
+        self.dictionary = None
+        self.label_types = None
 
-        self.x_train = self.training_data.x
-        self.y_train = keras.utils.to_categorical(self.training_data.y,
-                                                  self.num_classes)
+    def load(self):
+        self.model = load_model("models/nn_fc_model.h5")
+        with open("models/labels.pickle", "r") as f:
+            self.label_types = pickle.load(f)
+        with open("models/inputs.dict", "r") as f:
+            self.dictionary = pickle.load(f)
 
-        # use dictionary constructed by training data
-        self.test_data = vectorizer.Vectorizer(tests,
-                                               self.training_data.dictionary,
-                                               self.training_data.label_types)
-        self.x_test = self.test_data.x
-        self.y_test = keras.utils.to_categorical(self.test_data.y,
-                                                 self.num_classes)
+    def save(self):
+        if not os.path.exists("models"):
+            os.makedirs("models")
+        self.model.save("models/nn_fc_model.h5")
+        with open("models/labels.pickle", "wb") as f:
+            pickle.dump(self.label_types, f)
+        with open("models/inputs.dict", "wb") as f:
+            self.dictionary.save(f)
 
-        self.model = self.__construct()
-        self.model.save("nn_fc_model.h5")
-        self.train()
+    def inference_html(self, record):
+        (word_vecs, _) = self.__convert_to_word_vecs([record])
+        (numpy_vecs, _) = self.__adjust_format(word_vecs)
+        label_id = self.model.predict(numpy_vecs)[0].argmax()
+        return self.label_name_from_id(label_id)
 
-    def train(self):
-        batch_size = 200
-        epochs = 400
+    def label_name_from_id(self, label_id):
+        return self.label_types[label_id]
+
+    def train(self, training, test, epochs=400):
+        self.__prepare_for_training(training, test)
+        self.model = self.__construct_neural_network()
         self.model.fit(self.x_train, self.y_train,
-                       batch_size=batch_size,
+                       batch_size=self.batch_size,
                        epochs=epochs,
                        verbose=1,
                        validation_data=(self.x_test, self.y_test))
@@ -47,21 +58,53 @@ class NNFullyConnectedModel:
         print('Test loss:', score[0])
         print('Test accuracy', score[1])
 
-    def inference_html(self, record):
-        (word_vecs, _) = self.training_data.convert_to_word_vecs([record])
-        numpy_vecs = self.training_data.convert_to_numpy_vecs(word_vecs)
-        label_id = self.model.predict(numpy_vecs)[0].argmax()
-        return self.training_data.label_name_from_id(label_id)
+    def __prepare_for_training(self, training, tests):
+        """
+        set self.dictionary, self.lable_types and
+        generate train_x(y) and test_x(y)
+        """
+        (word_vecs_train, labels_train) = self.__convert_to_word_vecs(training)
+        (word_vecs_test, labels_test) = self.__convert_to_word_vecs(tests)
 
-    def __construct(self):
+        # use dictionary and label_types of training set
+        self.dictionary = corpora.Dictionary(word_vecs_train)
+        self.label_types = list(set(labels_train))
+
+        (self.x_train,
+         self.y_train) = self.__adjust_format(word_vecs_train, labels_train)
+        (self.x_test,
+         self.y_test) = self.__adjust_format(word_vecs_test, labels_test)
+
+    def __adjust_format(self, word_vecs, labels=None):
+        bows = [self.dictionary.doc2bow(v) for v in word_vecs]
+        x = matutils.corpus2dense(bows, len(self.dictionary.keys())).T
+
+        y = None
+        if labels is not None:
+            y = [self.label_types.index(l) for l in labels]
+            y = np.asarray(y, dtype=np.float32)
+            y = keras.utils.to_categorical(y, len(self.label_types))
+        return (x, y)
+
+    def __convert_to_word_vecs(self, records):
+        input_tag_tokenizer = tokenizer.InputTagTokenizer()
+        word_vecs = []
+        labels = []
+        test_labels = []
+        for r in records:
+            word_vecs.append(input_tag_tokenizer.get_attrs_value(r.html))
+            labels.append(r.label)
+        return (word_vecs, labels)
+
+    def __construct_neural_network(self):
         model = Sequential()
         model.add(Dense(400,
                         activation='relu',
-                        input_shape=(self.num_terms,)))
+                        input_shape=(len(self.dictionary.keys()),)))
         model.add(Dropout(0.5))
         model.add(Dense(100, activation='relu'))
         model.add(Dropout(0.5))
-        model.add(Dense(self.num_classes, activation='softmax'))
+        model.add(Dense(len(self.label_types), activation='softmax'))
         model.compile(loss=keras.losses.categorical_crossentropy,
                       optimizer=keras.optimizers.Adadelta(),
                       metrics=['accuracy'])
