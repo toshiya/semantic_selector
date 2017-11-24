efa808505c9607f1c07592cff393e2ecaade61e5
Toshiya Komoda <toshiya.komoda@gmail.com>
2017-11-25 01:41:43 +0900

    fix: refactor test_adapter

--- projects/tests/test_adapter.py
+++ projects/tests/test_adapter.py
@@ -11,24 +11,20 @@ class TestMySQLTrainingAdapter(unittest.TestCase):
         InputTagTokenizer()
 
     @mock.patch('semantic_selector.mysql.InputTable.fetch_data')
-    @mock.patch('semantic_selector.mysql.Input.canonical_topic',
-                new_callable=mock.PropertyMock)
     @mock.patch('semantic_selector.tokenizer.'
                 'InputTagTokenizer.get_attrs_value')
     def test_generate_training_data(self,
                                     mock_get_attrs_value,
-                                    mock_canonical_topic,
                                     mock_fetch_data):
 
         training = [Input(id=1, url='url', html='html',
-                          parent_html='parent_html', topic='topic1'),
+                          parent_html='parent_html', topic='pc_email'),
                     Input(id=2, url='url', html='html',
-                          parent_html='parent_html', topic='topic2')]
+                          parent_html='parent_html', topic='password')]
         tests = [Input(id=3, url='url', html='html',
-                       parent_html='parent_html', topic='topic1')]
+                       parent_html='parent_html', topic='pc_email')]
 
         mock_get_attrs_value.return_value = ["a", "b"]
-        mock_canonical_topic.return_value = "password"
         mock_fetch_data.return_value = (training, tests)
 
         options = {'ratio_test': 0.3, 'threashold': 0, 'seed': 100}
@@ -37,7 +33,8 @@ class TestMySQLTrainingAdapter(unittest.TestCase):
         self.assertTrue('a' in list(adapter.dictionary.values()))
         self.assertTrue('b' in list(adapter.dictionary.values()))
 
-        self.assertEqual(len(adapter.all_topics), 1)
+        self.assertEqual(len(adapter.all_topics), 2)
+        self.assertTrue('email' in list(adapter.all_topics))
         self.assertTrue('password' in list(adapter.all_topics))
 
 
