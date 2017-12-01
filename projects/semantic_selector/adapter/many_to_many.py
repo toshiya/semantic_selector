1b1133961dd1454a5aefe14bd8c192b5e397241a
Toshiya Komoda <toshiya.komoda@gmail.com>
2017-12-02 01:29:27 +0900

    feature: implement pad

--- projects/semantic_selector/adapter/many_to_many.py
+++ projects/semantic_selector/adapter/many_to_many.py
@@ -1,24 +1,31 @@
 from abc import abstractmethod
 import numpy as np
 from gensim import corpora
+from semantic_selector.mysql import InputTable
 from semantic_selector.adapter.one_to_one import Adapter, get_attribute
+
 
 class ManyToManyAdapter(Adapter):
     pass
 
+
 class TrainingAdapter(ManyToManyAdapter):
     def __init__(self, options):
+        (train, test) = self.get_raw_data(options)
         (self.x_train,
          self.y_train,
          self.x_test,
          self.y_test,
          self.max_num_input_tags,
          self.dictionary,
-         self.all_topics) = self.generate_training_data(options)
+         self.all_topics) = self.make_training_data(train, test)
 
-    # TODO
-    def pad(self, vector, shape):
-        pass
+    # shape must be 2 dimensional
+    def pad(self, vec, shape, dtype):
+        for i in range(0, shape[0] - len(vec)):
+            v = np.zeros((1, shape[1]), dtype=dtype)
+            vec = np.append(vec, v, axis=0)
+        return vec
 
     def make_dictionary(self, records):
         word_vecs = self.convert_to_word_vecs(records)
@@ -29,25 +36,28 @@ class TrainingAdapter(ManyToManyAdapter):
         return list(set(topic_vecs))
 
     def make_pages(self, records):
+        if len(records) == 0:
+            return ([], 0)
         pages = []
         previous_url = None
         current_page = []
         max_num_input_tags = 0
         for record in records:
             url = get_attribute(record, 'url')
-            if (previous_url != url) and (current_page is not None):
+            if (previous_url != url) and (len(current_page) != 0):
                 if max_num_input_tags < len(current_page):
                     max_num_input_tags = len(current_page)
-                    pages.append(current_page)
-                    current_page = []
+                pages.append(current_page)
+                current_page = []
             current_page.append(record)
+            previous_url = url
         pages.append(current_page)
         return (pages, max_num_input_tags)
 
     def convert_to_x_y(self, pages,
-                             max_num_input_tags,
-                             dictionary,
-                             all_topics):
+                       max_num_input_tags,
+                       dictionary,
+                       all_topics):
         dict_size = len(dictionary.keys())
         topic_size = len(all_topics)
         x = np.empty((0, max_num_input_tags, dict_size), dtype='float64')
@@ -55,25 +65,20 @@ class TrainingAdapter(ManyToManyAdapter):
         for page in pages:
             word_vecs = self.convert_to_word_vecs(page)
             numpy_vecs = self.adjust_x_format(dictionary, word_vecs)
-            numpy_vecs = self.pad(numpy_vecs, (max_num_input_tags, dict_size))
+            numpy_vecs = self.pad(numpy_vecs,
+                                  (max_num_input_tags, dict_size),
+                                  'float64')
             x = np.append(x, np.array([numpy_vecs]), axis=0)
 
-            topic_vecs = self.convert_to_word_vecs(page)
-            numpy_topic_vecs = self.adjust_x_format(all_topics, topic_vecs)
+            topic_vecs = self.convert_to_topic_vecs(page)
+            numpy_topic_vecs = self.adjust_y_format(all_topics, topic_vecs)
             numpy_topic_vecs = self.pad(numpy_topic_vecs,
-                                        (max_num_input_tags, topic_size))
+                                        (max_num_input_tags, topic_size),
+                                        'float64')
             y = np.append(y, np.array([numpy_topic_vecs]), axis=0)
         return (x, y)
 
-    @abstractmethod
-    def generate_training_data(self, options):
-        pass
-
-class JSONTrainingAdapter(TrainingAdapter):
-    def generate_training_data(self, options):
-        train = options['samples']['train']
-        test = options['samples']['test']
-
+    def make_training_data(self, train, test):
         dictionary = self.make_dictionary(train)
         all_topics = self.make_all_topics(train)
 
@@ -93,3 +98,22 @@ class JSONTrainingAdapter(TrainingAdapter):
 
         return (x_train, y_train, x_test, y_test,
                 max_num_input_tags, dictionary, all_topics)
+
+    @abstractmethod
+    def get_raw_data(self, options):
+        pass
+
+
+class JSONTrainingAdapter(TrainingAdapter):
+    def get_raw_data(self, options):
+        train = options['samples']['train']
+        test = options['samples']['test']
+        return (train, test)
+
+
+class MySQLTrainingAdapter(TrainingAdapter):
+    def get_raw_data(self, options):
+        input_table = InputTable(options['threashold'])
+        (train, test) = input_table.fetch_data(options['ratio_test'],
+                                               options['seed'])
+        return (train, test)
--- projects/tests/test_adapter_many_to_many.py
+++ projects/tests/test_adapter_many_to_many.py
@@ -1,4 +1,5 @@
 import unittest
+from semantic_selector.tokenizer import InputTagTokenizer
 from semantic_selector.adapter.many_to_many import JSONTrainingAdapter
 
 
@@ -6,18 +7,18 @@ class TestJSONTrainingAdapter(unittest.TestCase):
     email_html = "<input type='text' name='mail_addr' placeholder='メールアドレス'>"
     password_html = "<input type='password' name='pass' placeholder='パスワード'>"
 
-    data = { "train": [
+    data = {"train": [
                         {
                             'id': 1,
                             'title': 'site1',
-                            'url': "http//example1.com/register",
+                            'url': "http://example1.com/register",
                             'html': email_html,
                             'canonical_topic': 'email',
                         },
                         {
                             'id': 2,
                             'title': 'site1',
-                            'url': "http//example1.com/register",
+                            'url': "http://example1.com/register",
                             'html': password_html,
                             'canonical_topic': 'password',
                         },
@@ -26,23 +27,29 @@ class TestJSONTrainingAdapter(unittest.TestCase):
                             'title': 'site2',
                             'url': "http//example2.com/register",
                             'html': email_html,
-                            'canonical_topic': 'password',
+                            'canonical_topic': 'email',
                         },
                         {
                             'id': 4,
                             'title': 'site3',
                             'url': "http//example3.com/register",
                             'html': email_html,
-                            'canonical_topic': 'password',
+                            'canonical_topic': 'email',
                         },
                     ],
-             "test": [],
+            "test": [
+                        {
+                            'id': 5,
+                            'title': 'site4',
+                            'url': "http//example4.com/register",
+                            'html': email_html,
+                            'canonical_topic': 'email',
+                        },
+                    ]
             }
-
 
     def SetUp(self):
         InputTagTokenizer()
-
 
     def test_generate_training_data(self):
         options = {
@@ -52,13 +59,16 @@ class TestJSONTrainingAdapter(unittest.TestCase):
         }
         adapter = JSONTrainingAdapter(options)
 
-        dict_size = adapter.dictionary.keys()
+        dict_size = len(adapter.dictionary.keys())
         self.assertEqual(dict_size, 7)
 
         max_num_input_tags = adapter.max_num_input_tags
         self.assertEqual(max_num_input_tags, 2)
 
         self.assertEqual(adapter.x_train.shape, (3, max_num_input_tags, 7))
+        self.assertEqual(adapter.y_train.shape, (3, max_num_input_tags, 2))
+        self.assertEqual(adapter.x_test.shape, (1, max_num_input_tags, 7))
+        self.assertEqual(adapter.y_test.shape, (1, max_num_input_tags, 2))
 
 
 if __name__ == '__main__':
