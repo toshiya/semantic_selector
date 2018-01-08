0fc27d28ea98946a159fbbf8a767934ddd1f088d
Toshiya Komoda <toshiya.komoda@gmail.com>
2018-01-08 16:39:38 +0900

    feature: move test data selection logic to adapter.

--- projects/semantic_selector/adapter/training.py
+++ projects/semantic_selector/adapter/training.py
@@ -2,7 +2,7 @@ from abc import abstractmethod
 from gensim import corpora
 from semantic_selector.mysql import InputTable
 from .base import Adapter
-
+import numpy as np
 
 class TrainingAdapter(Adapter):
     def __init__(self, options):
@@ -24,24 +24,41 @@ class MySQLTrainingAdapter(TrainingAdapter):
         generate train_x(y) and test_x(y)
         """
         input_table = InputTable(options['threashold'])
-        (training, test) = input_table.fetch_data(options['ratio_test'],
-                                                  options['seed'])
+        data = input_table.fetch_data()
 
-        word_vecs_train = self.convert_to_word_vecs(training)
-        topic_vecs_train = self.convert_to_topic_vecs(training)
-        word_vecs_test = self.convert_to_word_vecs(test)
-        topic_vecs_test = self.convert_to_topic_vecs(test)
+        word_vecs = self.convert_to_word_vecs(data)
+        topic_vecs = self.convert_to_topic_vecs(data)
 
         # use dictionary and topic_types of training set
         wv_flatten = []
-        for x in word_vecs_train:
+        for x in word_vecs:
             wv_flatten.extend(x)
         self.dictionary = corpora.Dictionary(wv_flatten)
 
         tp_flatten = []
-        for x in topic_vecs_train:
+        for x in topic_vecs:
             tp_flatten.extend(x)
         self.all_topics = list(set(tp_flatten))
+
+        # select test data
+        num_pages = len(word_vecs)
+        print("number of total pages: " + str(num_pages))
+        np.random.seed(options['seed'])
+        ratio_test = options['ratio_test']
+        num_test_pages = int(num_pages * ratio_test)
+        test_indices = np.random.permutation(num_pages)[0:num_test_pages]
+
+        word_vecs_train = []
+        topic_vecs_train = []
+        word_vecs_test = []
+        topic_vecs_test = []
+        for i in range(0, num_pages):
+            if i not in test_indices:
+                word_vecs_train.append(word_vecs[i])
+                topic_vecs_train.append(topic_vecs[i])
+            else:
+                word_vecs_test.append(word_vecs[i])
+                topic_vecs_test.append(topic_vecs[i])
 
         be_train = self.to_bow_element_vectors(word_vecs_train)
         ot_train = self.to_one_hot_topic_vectors(topic_vecs_train)
